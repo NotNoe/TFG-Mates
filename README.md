f50313562e4212018904c3c20deb2f19f8060419
Noelia Barranco Godoy <56674140+NotNoe@users.noreply.github.com>
2025-05-08 18:24:12 +0200

    finales

--- README.md
+++ README.md
@@ -14,11 +14,29 @@ Ana María Carpio Rodríguez
 
 ## Resumen
 
+El estudio y la simulación numérica de fenómenos físicos descritos por ecuaciones en derivadas parciales (EDPs) constituye una herramienta fundamental en múltiples disciplinas científicas y de ingeniería. Sin embargo, la resolución eficiente de estos problemas puede volverse computacionalmente costosa cuando se requieren mallas finas o dominios de gran tamaño. En este contexto, la programación en GPU surge como una alternativa poderosa para acelerar algoritmos numéricos gracias a su arquitectura de paralelismo masivo.
+
+En este trabajo se analizan e implementan métodos numéricos para resolver cinco EDPs clásicas —la ecuación del calor (1D y 2D), la ecuación de onda (1D y 2D) y la ecuación de Laplace (2D)—  mediante esquemas de diferencias finitas. Tras establecer el marco teórico, se desarrollan implementaciones tanto en CPU como en GPU utilizando Python y PyCUDA.
+
+El objetivo principal es cuantificar el aumento de rendimiento obtenido al ejecutar estos algoritmos en GPU. Para ello, se construye un benchmark y se comparan los tiempos de ejecución en ambos algoritmos. Los resultados muestran mejoras significativas al utilizar GPU, con aceleraciones de hasta dos órdenes de magnitud en algunos casos. Finalmente, se discuten las limitaciones observadas y se proponen posibles líneas de optimización y trabajo futuro.
 
 ## Abstract
+The study and numerical simulation of physical phenomena described by partial differential equations (PDEs) is a fundamental tool in many scientific and engineering disciplines. However, solving these problems efficiently can become computationally expensive when fine meshes or large domains are required. In this context, GPU programming emerges as a powerful alternative to accelerate numerical algorithms, thanks to its massively parallel architecture.
 
+This work analyzes and implements numerical methods to solve five classical PDEs —the heat equation (1D and 2D), the wave equation (1D and 2D), and the Laplace equation (2D)— using finite difference schemes. After establishing the theoretical framework, we develop implementations both on CPU and GPU using Python and PyCUDA.
+
+The main objective is to quantify the performance improvement achieved by executing these algorithms on a GPU. To this end, a benchmark is built and execution times are compared between both approaches. The results show significant gains when using GPU, with speedups of up to two orders of magnitude in some cases. Finally, observed limitations are discussed and possible optimization paths and future work are proposed.
 
 
 ## Estructura del repositorio
 
 - **TFG.pdf**: El pdf con el trabajo.
+- **cpu/**: Implementación de los métodos numéricos en CPU.
+- **gpu/**: Implementación de los métodos numéricos en GPU.
+- **CUDA/**: Archivos .cu con los kernels de CUDA.
+- **benchmark/**: Infraestructura para ejecutar tests y almacenar los tiempos.
+- **graphs/**: Scripts para generar las gráficas utilizadas en el TFG.
+- **tests/**: Scripts para la validación numérica de los algoritmos.
+- **demos/**: Ejemplos ilustrativos de programas sencillos de PyCUDA.
+- **utils/**: Utilidades para compilar kernels.
+- **env.yml/**: Archivo con la configuración del entorno Conda.
---
+++ graphs/report.py
@@ -0,0 +1,140 @@
+import json
+from matplotlib.lines import Line2D
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+ECUATIONS_TO_NAME = {"heat1d": "Ecuación del Calor 1D",
+                     "laplace": "Ecuación de Laplace 2D",
+                     "heat2d": "Ecuación del Calor 2D",
+                     "wave1d": "Ecuación de Onda 1D",
+                     "wave2d": "Ecuación de Onda 2D"}
+
+def generate_df(report_path):
+    with open(report_path, 'r') as f:
+        cases = json.load(f)["cases"]
+
+    df = pd.DataFrame(cases)
+    args_df = pd.json_normalize(df["args"]).astype({"n_x": pd.Int32Dtype(),
+                                                    "n_y": pd.Int32Dtype(),
+                                                    "n_t": pd.Int32Dtype()})
+                                        
+    metrics_df = pd.json_normalize(df["metrics"])
+    metrics_df = metrics_df.astype({"t_wall": pd.Float32Dtype(),
+                                    "error": pd.Float32Dtype()})
+    df = pd.concat([df.drop(columns=["args", "metrics"]), args_df, metrics_df], axis=1)
+    df['mesh_nodes'] = df[['n_x', 'n_y']].prod(1, True)
+    return df
+
+def graf_speedup(df, ax=None):
+    pivot = df.pivot(index="mesh_nodes", columns="device", values="t_wall").sort_index()
+    pivot["speedup"] = pivot["cpu"] / pivot["gpu"]
+    x = pivot.index
+    y = pivot["speedup"]
+    logx = np.log(x)
+    logy = np.log(y)
+    b, loga = np.polyfit(logx, logy, 1)       # ajuste lineal en log-log
+    a = np.exp(loga)
+
+    # luego, sobre tu gráfica:
+    xx = np.linspace(x.min(), x.max(), 100)
+    yy = a * xx**b
+    if ax is None:
+        fig, ax = plt.subplots(figsize=(8, 5))
+    ax.plot(xx, yy, linestyle=':', label=f'Tendencia $\\propto x^{{{b:.2f}}}$')
+    ax.plot(pivot.index, pivot["speedup"], marker="o", linestyle="-")
+    ax.set_xscale("log")
+    #plt.yscale("log")
+    ax.grid(True, which="major", ls="--", lw=0.5)
+    return ax
+
+def graf_error(df, ax = None):
+    pivot_err = (
+        df
+        .pivot(index="mesh_nodes", columns="device", values="error")
+        .sort_index()
+    )
+    if ax is None:
+        fig, ax = plt.subplots(figsize=(8, 5))
+
+    # Dibujar Error vs mesh_nodes
+    ax.plot(pivot_err.index, pivot_err["cpu"], marker="o", linestyle="-", label="CPU")
+    ax.plot(pivot_err.index, pivot_err["gpu"], marker="o", linestyle="-", label="GPU")
+    ax.set_xscale("log")
+    ax.set_yscale("log")  # Error often plotted log-log to see rates
+    ax.set_xlabel("Número de nodos de malla")
+    ax.set_ylabel("Error numérico")
+    ax.set_title("Error vs. Número de nodos para la Ecuación del Calor")
+    ax.grid(which="major", linestyle="--", linewidth=0.5)
+    legend_handles = [
+        Line2D([0], [0], color='C0', lw=2),
+        Line2D([0], [0], color='C1', lw=2)
+    ]
+    ax.legend(legend_handles, ["CPU", "GPU"])
+    return ax
+
+def graf_time(df, ax=None):
+    pivot_time = (
+        df
+        .pivot(index="mesh_nodes", columns="device", values="t_wall")
+        .sort_index()
+    )
+    if ax is None:
+        fig, ax = plt.subplots(figsize=(8, 5))
+
+    # 6) Dibujar tiempo vs mesh_nodes en escala log-log
+    ax.plot(pivot_time.index, pivot_time["cpu"], marker="o", linestyle="-", label="CPU")
+    ax.plot(pivot_time.index, pivot_time["gpu"], marker="o", linestyle="-", label="GPU")
+
+    ax.set_xscale("log")
+    #ax.set_yscale("log")
+    ax.grid(which="major", linestyle="--", linewidth=0.5)
+    legend_handles = [
+        Line2D([0], [0], color='C0', lw=2),
+        Line2D([0], [0], color='C1', lw=2)
+    ]
+    ax.legend(legend_handles, ["CPU", "GPU"])
+
+    return ax
+
+
+
+if __name__ == "__main__":
+    df = generate_df("out/report.json")
+    fig_time, axs_time = plt.subplots(2, 3, figsize=(12, 8))
+    axs_time = axs_time.flatten()
+    equations = df["equation"].unique()
+    for ax, eq in zip(axs_time, equations):
+        df_eq = df[df["equation"] == eq]
+        graf_time(df_eq, ax=ax)      # Usa tu función para graficar tiempos
+        ax.set_title(ECUATIONS_TO_NAME[eq])
+    # Ocultar el subplot extra
+    axs_time[-1].axis("off")
+    fig_time.supxlabel("Número de nodos de malla")
+    fig_time.supylabel("Tiempo de ejecución (s)")
+    fig_time.tight_layout(rect=[0, 0.03, 1, 0.95])
+    plt.savefig('out/t_wall.png')
+
+    # 2) Figura de speed-up
+    fig_sp, axs_sp = plt.subplots(2, 3, figsize=(12, 8))
+    axs_sp = axs_sp.flatten()
+    for ax, eq in zip(axs_sp, equations):
+        df_eq = df[df["equation"] == eq]
+        graf_speedup(df_eq, ax=ax)   # Usa tu función para graficar speed-up
+        ax.set_title(ECUATIONS_TO_NAME[eq])
+    # Ocultar el subplot extra
+    axs_sp[-1].axis("off")
+    fig_sp.supxlabel("Número de nodos de malla")
+    fig_sp.supylabel("Speed-up (CPU / GPU)")
+    fig_sp.tight_layout(rect=[0, 0.03, 1, 0.95])
+    plt.savefig('out/speedup.png')
+
+    # 2) Figura de speed-up
+    fig_sp, axs_sp = plt.subplots(2, 3, figsize=(12, 8))
+    axs_sp = axs_sp.flatten()
+    for ax, eq in zip(axs_sp, equations):
+        df_eq = df[df["equation"] == eq]
+        graf_error(df_eq, ax=ax)   # Usa tu función para graficar speed-up
+        ax.set_title(ECUATIONS_TO_NAME[eq])
+    axs_sp[-1].axis("off")
+    #plt.show()
